369397b2a5bf209712410d4c08826cc8ffe70d68
Brandon Weaver <a7xncob@gmail.com>
2020-02-24 21:28:21 +0000

    Automatically backed up by Learn

--- app/controllers/birds_controller.rb
+++ app/controllers/birds_controller.rb
@@ -6,7 +6,7 @@ class BirdsController < ApplicationController
   end
   
   def show
-    bird = Bird.find_by_id(params[:id])
+    bird = Bird.find_by(id: params[:id])
     render json: bird.slice(:id, :name, :species)
   end
   
